08da371d100d9b65380aa27862bca2d41457f4e2
lp1506947671 <1506946771@qq.com>
2021-04-19 22:06:19 +0800

    [类型]:需求开发 [描述]:博客系统之数据库表设计 [提交人]:Jason

--- myblog/blog/models.py
+++ myblog/blog/models.py
@@ -1,3 +1,95 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
 
 # Create your models here.
+
+
+class UserInfo(AbstractUser):
+    nid = models.AutoField(primary_key=True)
+    telephone = models.CharField(max_length=11, null=True, unique=True)
+    avatar = models.FileField(upload_to='avatars/', default="avatars/default.png")
+    create_time = models.DateTimeField(verbose_name="创建时间", auto_now_add=True)
+    blog = models.OneToOneField(to='Blog', to_field='nid', null=True, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.username
+
+
+class Blog(models.Model):
+    nid = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=64, verbose_name="个人博客标题")
+    site_name = models.CharField(max_length=64, verbose_name="站点名称")
+    theme = models.CharField(max_length=64, verbose_name="博客主题")
+
+    def __str__(self):
+        return self.title
+
+
+class Article(models.Model):
+    nid = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=64, verbose_name="文章标题")
+    create_time = models.CharField(max_length=256, verbose_name="创建时间")
+    content = models.TextField()
+    comment_count = models.IntegerField(default=0)
+    up_count = models.IntegerField(default=0)
+    down_count = models.IntegerField(default=0)
+    user = models.ForeignKey(to="UserInfo", to_field="nid", on_delete=models.CASCADE, verbose_name='作者')
+    category = models.ForeignKey(to="Category", to_field="nid", on_delete=models.CASCADE, null=True)
+    tags = models.ManyToManyField(to="Tag", through="Articles2Tag", through_fields=("article", 'tag'))
+
+    def __str__(self):
+        return self.title
+
+
+class ArticlesUpDown(models.Model):
+    nid = models.AutoField(primary_key=True)
+    user = models.ForeignKey(to="UserInfo", to_field="nid", on_delete=models.CASCADE, verbose_name="评论者")
+    article = models.ForeignKey(to="Article", to_field="nid", on_delete=models.CASCADE, verbose_name="被评论的文章")
+    is_up = models.BooleanField(default=True)
+
+    class Meta:
+        constraints = [models.UniqueConstraint(fields=['user', "article"], name="user_article_up_down")]
+
+
+class Comment(models.Model):
+    nid = models.AutoField(primary_key=True)
+    user = models.ForeignKey(to="UserInfo", to_field="nid", on_delete=models.CASCADE, verbose_name="评论者")
+    article = models.ForeignKey(to="Article", to_field="nid", on_delete=models.CASCADE, verbose_name="被评论的文章")
+    create_time = models.DateTimeField(auto_now_add=True, verbose_name="创建时间")
+    content = models.CharField(max_length=512, verbose_name="评论内容")
+    parent_comment = models.ForeignKey(to="self", null=True, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.content
+
+
+class Category(models.Model):
+    nid = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=128, verbose_name="分类标题")
+    blog = models.ForeignKey(to='Blog', to_field='nid', on_delete=models.CASCADE, verbose_name="所属博客")
+
+    def __str__(self):
+        return self.title
+
+
+class Tag(models.Model):
+    nid = models.AutoField(primary_key=True)
+    title = models.CharField(verbose_name='标签名称', max_length=32)
+    blog = models.ForeignKey(verbose_name='所属博客', to='Blog', to_field='nid', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+
+class Articles2Tag(models.Model):
+    nid = models.AutoField(primary_key=True)
+    article = models.ForeignKey(verbose_name='文章', to="Article", to_field='nid', on_delete=models.CASCADE)
+    tag = models.ForeignKey(verbose_name='标签', to="Tag", to_field='nid', on_delete=models.CASCADE)
+
+    class Meta:
+        constraints = [models.UniqueConstraint(fields=['article', 'tag'], name="article_tag")]
+
+    def __str__(self):
+        v = self.article.title + "---" + self.tag.title
+        return v
--- myblog/myblog/settings.py
+++ myblog/myblog/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = 'django-insecure-pcei)k1qd@oq9xn0@*a86z)!u3mmqf0t-r-lpf6+#qfr#0)#wn
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -71,17 +69,19 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'myblog.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'blog',
+        'USER': 'root',
+        'PASSWORD': '666666',
+        'HOST': '127.0.0.1',
+        'PORT': 3306
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -101,7 +101,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
@@ -115,7 +114,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
@@ -126,3 +124,5 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = "blog.UserInfo"
